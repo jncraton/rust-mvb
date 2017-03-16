d79da74eb07b929a9488f471085ad786e7712b25
Jon Craton <jncraton@gmail.com>
2017-03-16 15:16:41 -0400

    removed old example code

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use std::io::prelude::*;
 use std::fs::File;
 
 #[derive(Clone, Copy)]
-struct Echo;
+struct Server;
 
 fn replace_with_file(needle: &str, filename: &str, haystack: String) -> String {
   let mut file = File::open(filename).unwrap();
@@ -25,37 +25,26 @@ fn replace_with_file(needle: &str, filename: &str, haystack: String) -> String {
   return haystack.replace(needle, &content);
 }
 
-impl Service for Echo {
+impl Service for Server {
     type Request = Request;
     type Response = Response;
     type Error = hyper::Error;
     type Future = FutureResult<Response, hyper::Error>;
 
     fn call(&self, req: Request) -> Self::Future {
-        futures::future::ok(match (req.method(), req.path()) {
-            (&Get, "/") | (&Get, "/echo") => {
-                let mut file = File::open("template.html").unwrap();
-                let mut template = String::new();
-                file.read_to_string(&mut template).unwrap();
+        futures::future::ok({
+            // req.method() req.path()
+            let mut file = File::open("template.html").unwrap();
+            let mut template = String::new();
+            file.read_to_string(&mut template).unwrap();
 
-                template = replace_with_file("{{ content }}", "pages/content.md", template);
-                template = replace_with_file("{{ style }}", "style.css", template);
+            template = replace_with_file("{{ content }}", "pages/content.md", template);
+            template = replace_with_file("{{ style }}", "style.css", template);
 
-                Response::new()
-                    .with_header(ContentLength(template.len() as u64))
-                    .with_body(template)
-            },
-            (&Post, "/echo") => {
-                let mut res = Response::new();
-                if let Some(len) = req.headers().get::<ContentLength>() {
-                    res.headers_mut().set(len.clone());
-                }
-                res.with_body(req.body())
-            },
-            _ => {
-                Response::new()
-                    .with_status(StatusCode::NotFound)
-            }
+            Response::new()
+                .with_header(ContentLength(template.len() as u64))
+                .with_body(template)
+            //Response::new().with_status(StatusCode::NotFound)
         })
     }
 
@@ -65,7 +54,7 @@ impl Service for Echo {
 fn main() {
     let addr = "127.0.0.1:1337".parse().unwrap();
 
-    let server = Http::new().bind(&addr, || Ok(Echo)).unwrap();
+    let server = Http::new().bind(&addr, || Ok(Server)).unwrap();
     println!("Listening on http://{} with 1 thread.", server.local_addr().unwrap());
     server.run().unwrap();
 }
